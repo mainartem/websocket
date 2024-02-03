914b8e4a342acec850b9bff0005e2676a17bc4c8
Artem_Chernobuk <103370001+mainartem@users.noreply.github.com>
2024-02-03 15:58:03 +0700

    Add files via upload

---
+++ Raspberry.py
@@ -0,0 +1,13 @@
+import socket
+import cv2
+HOST = '127.0.1.1' # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+capture = cv2.VideoCapture(0)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # Предположим, что video_stream - это ваш видеопоток
+    while True:
+        ret, video_stream = capture.read()
+        s.sendall(video_stream)
+
+capture.release()
